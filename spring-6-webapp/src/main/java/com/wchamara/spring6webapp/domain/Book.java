2fd14ba50475afaeaf56d08c7804f74e442d9abc
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-08-08 16:20:40 +0530

    adding getters and setters

--- spring-6-webapp/src/main/java/com/wchamara/spring6webapp/domain/Book.java
+++ spring-6-webapp/src/main/java/com/wchamara/spring6webapp/domain/Book.java
@@ -13,4 +13,28 @@ public class Book {
     private long id;
     private String title;
     private String isbn;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
